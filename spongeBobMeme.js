2b988783aadb102e4b88ede0e367fb6802e8be0a
Joshmul1 <36004913+Joshmul1@users.noreply.github.com>
2019-05-28 23:04:56 +0100

    Reformatted code to look nicer

--- spongeBobMeme.js
+++ spongeBobMeme.js
@@ -11,34 +11,33 @@ client.once('ready', () => {
     console.log(`Client Loaded`);
 });
 
-client.on(`message`, message =>{
-    if(isReady){
-        if(message.content.startsWith(`${prefix}`)){
-         isReady = false;
-         var messageContent = message.content.substring(prefix.length+1);
-         messageContent = messageContent.toLowerCase();
-         var newMessage='';
-         for (var i = 0; i < messageContent.length; i++) {
-            if(i%2===0){
-            newMessage = newMessage+ messageContent.charAt(i).toUpperCase();
-            }else{
-                newMessage = newMessage + messageContent.charAt(i);
+client.on(`message`, message => {
+    if (isReady) {
+        if (message.content.startsWith(`${prefix}`)) {
+            isReady = false;
+            var messageContent = message.content.substring(prefix.length + 1);
+            messageContent = messageContent.toLowerCase();
+            var newMessage = '';
+            for (var i = 0; i < messageContent.length; i++) {
+                if (i % 2 === 0) {
+                    newMessage = newMessage + messageContent.charAt(i).toUpperCase();
+                } else {
+                    newMessage = newMessage + messageContent.charAt(i);
                 }
             }
-         var channel = message.channel.id;
-         client.channels.get(channel).send(newMessage);
-         isReady = true;
+            var channel = message.channel.id;
+            client.channels.get(channel).send(newMessage);
+            isReady = true;
 
 
 
 
-
-          }
+        }
     }
 
 
 
-    });
+});
 
 
 
