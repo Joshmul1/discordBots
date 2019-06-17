b5ab9ac4c12e90a281700982e5055c12f3d6e984
Joshua Mulheron <joshua.mulheron.2016@uni.strath.ac.uk>
2019-06-17 19:09:10 +0100

    Added functionality to easily change between server regions EUW and EUC with appropriate help hints

--- spongeBobMeme.js
+++ spongeBobMeme.js
@@ -1,9 +1,13 @@
 var config = require('./config.json');
-var api_token = config.api_token;
-const prefix = '!sponge';
+var api_token = config.discord_api_token;
 const Discord = require(`discord.js`);
 var isReady = true;
 const client = new Discord.Client();
+
+
+// prefixes
+const sponge_prefix = '!sponge';
+const region_prefix = '!reg';
 
 client.once('ready', () => {
     console.log(`Client Loaded`);
@@ -11,9 +15,9 @@ client.once('ready', () => {
 
 client.on(`message`, message => {
     if (isReady) {
-        if (message.content.startsWith(`${prefix}`)) {
+        if (message.content.startsWith(`${sponge_prefix}`)) {
             isReady = false;
-            var messageContent = message.content.substring(prefix.length + 1);
+            var messageContent = message.content.substring(sponge_prefix.length + 1);
             messageContent = messageContent.toLowerCase();
             var newMessage = '';
             for (var i = 0; i < messageContent.length; i++) {
@@ -25,6 +29,27 @@ client.on(`message`, message => {
             }
             var channel = message.channel.id;
             client.channels.get(channel).send(newMessage);
+            isReady = true;
+        } else if (message.content.startsWith(`${region_prefix}`)) {
+            isReady = false;
+            var messageContent = message.content.substring(region_prefix.length + 1);
+            if (messageContent === 'EUW' || messageContent === 'euw') {
+                if (message.guild.region !== 'eu-west') {
+                    message.guild.setRegion('eu-west');
+                    message.channel.send("Server moved to EUW!");
+                } else {
+                    message.channel.send("The server is already in EU-West. Try send ``` !reg EUC ``` to move it to EU-Central instead");
+                }
+            } else if (messageContent === 'EUC' || messageContent === 'euc') {
+
+                if (message.guild.region !== 'eu-central') {
+                    message.guild.setRegion('eu-central');
+                    message.channel.send("Server moved to EUC!");
+
+                } else {
+                    message.channel.send("The server is already in EU-Central. Try send ``` !reg EUW ``` to move it to EU-West instead");
+                }
+            }
             isReady = true;
         }
     }
